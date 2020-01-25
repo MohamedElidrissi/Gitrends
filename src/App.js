4ca02c03a48f506d66d64561e34b964ea0a0bd30
Mohamed ELIDRISSI <45264848+MohamedElidrissi@users.noreply.github.com>
2020-01-25 11:11:27 +0100

    Add and apply a custom material-ui theme

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
+import theme from './theme';
 import Header from './components/Header';
 
 function App() {
   return (
     <CssBaseline>
-      <ThemeProvider>
+      <ThemeProvider theme={theme}>
         <Header />
       </ThemeProvider>
     </CssBaseline>
