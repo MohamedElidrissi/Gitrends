b52ecd9bb538a3dbc31025a68f5d37021e930e01
Mohamed ELIDRISSI <45264848+MohamedElidrissi@users.noreply.github.com>
2020-01-25 11:52:34 +0100

    Add "Not Found" page

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import NotFound from './pages/NotFound';
+
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={() => <h1>Home</h1>} />
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 );
