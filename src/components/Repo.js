5e5c03616e37c0c20d937b833543bbf75a0e5fa4
Mohamed ELIDRISSI <45264848+MohamedElidrissi@users.noreply.github.com>
2020-01-27 19:11:46 +0100

    Remove text-decoration from  button

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -6,7 +6,6 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
-import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 
 function Repo({ name, ownerName, ownerAvatar, desc, repoUrl }) {
@@ -28,7 +27,7 @@ function Repo({ name, ownerName, ownerAvatar, desc, repoUrl }) {
       )}
       <CardActions>
         <Button
-          component={Link}
+          component="a"
           href={repoUrl}
           target="_blank"
           rel="noopenner noreferrer"
